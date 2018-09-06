5f622a65e9ff2af3ad5eb48ea04303fba0751960
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-09-06 16:59:45 -0400

    Add project files.

---
+++ ReadWriteStream/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Writefile("Naz.txt");
+            Readfile("Naz.txt");
+        }
+       public static void Writefile(string filename)
+        {
+            StreamWriter write = new StreamWriter(filename);
+            write.WriteLine("Hi, Today You Created Write and Read File with System Input and Oupt");
+            write.Close();
+        }
+        public static void Readfile(string filename)
+        {
+            StreamReader read = new StreamReader(filename);
+            Console.WriteLine(read.ReadToEnd());
+            read.Close();
+
+        }
+    }
+}
